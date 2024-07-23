d7761a22c25969a3e179b7dd51a98badecb5169d
Fernando Villalba <21fervillalbag@gmail.com>
2024-07-23 18:12:33 -0400

    fix: config vite folder routes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  base: "./",
   build: {
     outDir: "./build",
+    emptyOutDir: true,
   },
 });
